e12f352c43a6bdeb70d47ae8f805ad2df9c8725c
EC2 Default User <ec2-user@ip-172-31-33-160.eu-west-1.compute.internal>
2022-02-09 00:52:03 +0000

    vnet added

---
+++ vnet.tf
@@ -0,0 +1,26 @@
+
+resource "azurerm_virtual_network" "vnet" {
+  name                ="vnetwork"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.0.0.0/16"]
+ #dns_servers         = ["10.0.0.4", "10.0.0.5"]
+
+  subnet {
+    name           = "subnet1"
+    address_prefix = "10.0.1.0/24"
+  }
+
+  subnet {
+    name           = "subnet2"
+    address_prefix = "10.0.2.0/24"
+   # security_group = azurerm_network_security_group.example.id
+  }
+
+subnet {
+    name           = "subnet3"
+    address_prefix = "10.0.3.0/24"
+   # security_group = azurerm_network_security_group.example.id
+  }
+ 
+}
