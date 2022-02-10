ecff2d4d1a221168391a639ac2324f57f38e203f
EC2 Default User <ec2-user@ip-172-31-33-160.eu-west-1.compute.internal>
2022-02-10 02:59:51 +0000

    subnet update

--- networkInt.tf
+++ networkInt.tf
@@ -4,8 +4,8 @@ resource "azurerm_network_interface" "NetInt1" {
   resource_group_name = azurerm_resource_group.rg.name
 
   ip_configuration {
-    name                          = "subnet1"
-    subnet_id                     = azurerm_subnet.vnet.id
+    name                          = "psubnet1"
+    subnet_id                     = azurerm_subnet.subnet1.id
     private_ip_address_allocation = "Dynamic"
   }
 }
