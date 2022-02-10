150da6e2ba4fec45be11df63f25f20a83ff6ea45
EC2 Default User <ec2-user@ip-172-31-33-160.eu-west-1.compute.internal>
2022-02-10 03:01:31 +0000

    vnet update

--- vnet.tf
+++ vnet.tf
@@ -28,18 +28,18 @@ resource "azurerm_virtual_network" "vnet" {
 resource "azurerm_subnet" "subnet1" {
   name                 = "psubnet1"
   resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
 resource "azurerm_subnet" "subnet2" {
   name                 = "psubnet2"
   resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.2.0/24"]
 }
 resource "azurerm_subnet" "subnet3" {
   name                 = "psubnet3"
   resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.3.0/24"]
 }
