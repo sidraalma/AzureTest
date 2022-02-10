b6bb68149c4ccd10edfaedab7b34273c38dbb97e
EC2 Default User <ec2-user@ip-172-31-33-160.eu-west-1.compute.internal>
2022-02-10 01:37:00 +0000

    updated vnet

--- vnet.tf
+++ vnet.tf
@@ -24,3 +24,27 @@ subnet {
   }
  
 }
+
+resource "azurerm_public_ip" "publicIP1" {
+  name                = "public1"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+#domain_name_label   = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_public_ip" "publicIP2" {
+  name                = "public2"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+ # domain_name_label   = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_public_ip" "publicIP3" {
+  name                = "public3"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+ # domain_name_label   = azurerm_resource_group.rg.name
+}
