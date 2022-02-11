d18ecbb62ad91dc43dc13b925dd26edd2c14c774
EC2 Default User <ec2-user@ip-172-31-33-160.eu-west-1.compute.internal>
2022-02-11 00:33:54 +0000

    minor1 updates

--- publicIP.tf
+++ publicIP.tf
@@ -19,7 +19,7 @@ resource "azurerm_public_ip" "publicIP3" {
   name                = "public3"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Dynmaic"
+  allocation_method   = "Dynamic"
   # domain_name_label   = azurerm_resource_group.rg.name
 }
 
